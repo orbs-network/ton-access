7e2d97d85d1701ab8f72bc1de27695c5434456e1
uvorbs <yuval@orbs.com>
2022-10-09 12:11:13 +0300

    before deploy patch

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 # from https://itnext.io/step-by-step-building-and-publishing-an-npm-typescript-package-44fe7164964c
 
+# IMPORTANT
+git checkout main
+
 npm version patch
 npm version minor
 npm version major
