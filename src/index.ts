5960e5bdc4a0b0b38f1ddb81ca6714939281a475
uvorbs <yuval@orbs.com>
2022-11-17 11:24:25 +0200

    2.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { Nodes } from "./nodes";
 
 type EdgeProtocol = "toncenter-api-v2" | "ton-api-v4" | "adnl-proxy"; // default: toncenter-api-v2
-type Network = "mainnet" | "testnet" //| "sandbox"- is deprecated ; // default: mainnet
+type Network = "mainnet" | "testnet"; //| "sandbox"- is deprecated ; // default: mainnet
 export interface Config {
   host?: string; // default: "ton.gateway.orbs.network"
   gatewayVersion?: number; // default: 1
   network?: Network;
   protocol?: "default" | "json-rpc" | "rest"; // default: "default"
-};
+}
 export class Gateway {
   //////////////////////////////////
   // config: Config;
@@ -30,7 +30,11 @@ export class Gateway {
     await this.nodes.init(`https://${this.host}/nodes`); // pass host when backend endpoint is ready
   }
   //////////////////////////////////
-  buildUrls(network?: Network, edgeProtocol?: EdgeProtocol, suffix?: string): string[] {
+  buildUrls(
+    network?: Network,
+    edgeProtocol?: EdgeProtocol,
+    suffix?: string
+  ): string[] {
     // default params
     if (!suffix) suffix = "";
 
@@ -50,7 +54,8 @@ export class Gateway {
 
 //////////////////////////////
 // private get multi endpoints
-async function getEndpoints(network?: Network,
+async function getEndpoints(
+  network?: Network,
   edgeProtocol?: EdgeProtocol,
   suffix?: string
 ): Promise<string[]> {
@@ -66,7 +71,7 @@ async function getEndpoints(network?: Network,
 // toncenter multi
 export async function getHttpEndpoints(config?: Config): Promise<string[]> {
   // default params
-  const network = config?.network ? config.network : "mainnet"
+  const network = config?.network ? config.network : "mainnet";
   let suffix = "jsonRPC";
   if (config?.protocol === "rest") {
     suffix = "";
@@ -84,10 +89,12 @@ export async function getHttpEndpoint(config?: Config): Promise<string> {
 // // API V4 - multi
 export async function getTonApiV4Endpoints(config?: Config): Promise<string[]> {
   // default params
-  const network = config?.network ? config.network : "mainnet"
+  const network = config?.network ? config.network : "mainnet";
 
-  if (config?.protocol === 'json-rpc') {
-    throw Error('config.protocol json-rpc is not supported for getTonApiV4Endpoints');
+  if (config?.protocol === "json-rpc") {
+    throw Error(
+      "config.protocol json-rpc is not supported for getTonApiV4Endpoints"
+    );
   }
   // any other case suffix should be empty
   const suffix = ""; // this is like rest - default
