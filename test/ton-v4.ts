af10f92e6f78013317f00c88242cc2ddb6dc613a
uvorbs <yuval@orbs.com>
2023-01-03 17:25:10 +0200

    add sanity for v4

--- test/ton-v4.ts
+++ test/ton-v4.ts
@@ -1,18 +1,38 @@
 import { expect } from 'chai';
-import { getHttpV4Endpoint } from '../src/index';
-import { TonClient4 } from "ton";
-import "isomorphic-fetch";
+import { getHttpV4Endpoint, getHttpV4Endpoints, Config, Network } from '../src/index';
 
+async function sanityEndpoint(endpoint: string) {
+    endpoint += 'block/latest';
+
+    //endpoint = "https://ton.access.orbs.network/4d8be7F95Bd3F8B62C092Ab4D238bEE463E655EE/1/mainnet/ton-api-v4/block/latest";
+    const settings = { method: "Get" };
+    const res = await fetch(endpoint);
+    const jsn = await res.json();
+    console.log('endpoint:', endpoint);
+    jsn.url = endpoint;
+    console.log('seqno', jsn.last.seqno);
+    expect(jsn.last.seqno).to.be.above(10000);
+}
+async function sanity(network: Network) {
+    let config: Config = { network };
+    // mainnet all nodes    
+    let endpoints = await getHttpV4Endpoints(config);
+    let calls: Array<Promise<void>> = [];
+    for (let endpoint of endpoints) {
+        calls.push(sanityEndpoint(endpoint))
+    }
+    await Promise.all(calls);
+    console.log('sanity done!');
+
+}
 describe('ton-V4', function () {
-    it('last should has valid seqno', async () => {
-        const endpoint = await getHttpV4Endpoint();
-        console.log("endpoint:", endpoint)
-        const client4 = new TonClient4({ endpoint });
-        let last = await client4.getLastBlock();
-        expect(last).to.not.be.undefined;
-        expect(last.last.seqno).to.be.above(24920000);
+    this.timeout(20000);
+    it('sanity - mainnet - ok status for all nodes', async () => {
+        await sanity('mainnet');
     });
-
+    it('sanity - mainnet - ok status for all nodes', async () => {
+        await sanity('testnet');
+    });
     ///block/latest
     it('explicit suffix latest block', async () => {
         let endpoint = await getHttpV4Endpoint();
