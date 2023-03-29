22610cb4b8144b1f523c2cdd710990f460e32cf3
uvorbs <yuval@orbs.com>
2023-03-29 14:55:51 +0300

    change LB test

--- test/infra.ts
+++ test/infra.ts
@@ -4,7 +4,7 @@ import { getHttpEndpoint, Config } from '../src/index';
 describe('Infra', function () {
     this.timeout(30000);
     // mainnet
-    it('should return roughly equal number per each node', async () => {
+    it('should return relevant number per each nodes weight', async () => {
         let counter: any = {};
         const config: Config = {
             network: 'mainnet'
@@ -23,10 +23,13 @@ describe('Infra', function () {
         console.log('sum', SUM);
         console.log('mean', mean);
         console.log(counter);
+        expect(mean).to.be.below(SUM / 2);
 
-        for (let id in counter) {
-            const div = counter[id] / mean;
-            expect(div).to.be.below(1.5);
-        }
+        // check load balancing is working and some get few traffic
+        // for (let id in counter) {
+        //     const div = counter[id] / mean;
+
+        //     expect(div).to.be.below(2);
+        // }
     });
 });
